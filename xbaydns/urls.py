71dded1859b8f481a07a8737df68692b6b0189a1
huangdong <huangdong@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-10-20 05:14:27 +0000

    加入数据库的sqlite的设置，并且初始化了管理员 :)

--- xbaydns/urls.py
+++ xbaydns/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     # (r'^xbaydns/', include('xbaydns.foo.urls')),
 
     # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+	(r'^admin/', include('django.contrib.admin.urls')),
 )
