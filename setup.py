fb1c7b4c5ffc8fe111f25ae5a029544f20e19af4
huangdong <huangdong@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-19 10:53:50 +0000

    把阿风疯狂的修改还原

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
 	download_url = 'http://xbaydns.googlecode.com',
 	zip_safe = False,
 
-	packages = find_packages(exclude=['*.tesllllts*']),
+	packages = find_packages(exclude=['*tests*']),
 	test_suite = 'xbaydns.tests.suite'
 )
