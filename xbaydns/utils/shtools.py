17215e62768ba9c7e674afd9e11446dcc41e1b72
huangdong <huangdong@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-19 11:08:57 +0000

    使用的bitten代码加入原作者的许可信息。同时把shtools中不用的代码删除

--- xbaydns/utils/shtools.py
+++ xbaydns/utils/shtools.py
@@ -4,6 +4,37 @@ shtools.py
 
 Created by 黄 冬 on 2007-11-07.
 Copyright (c) 2007 __MyCompanyName__. All rights reserved.
+
+Copyright © 2005-2007 Christopher Lenz[[BR]]
+Copyright © 2007 Edgewall Software[[BR]]
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+ 3. The name of the author may not be used to endorse or promote
+    products derived from this software without specific prior
+    written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR “AS IS” AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """
 
 import logging
@@ -13,42 +44,6 @@ import shlex
 from xbaydns.utils.command import CommandLine
 
 log = logging.getLogger('perbay.utils.shtools')
-
-def exec_(executable=None, file_=None, output=None, args=None):
-    """Execute a program or shell script.
-    
-    :param executable: name of the executable to run
-    :param file\_: name of the script file, relative to the project directory,
-                  that should be run
-    :param output: name of the file to which the output of the script should be
-                   written
-    :param args: command-line arguments to pass to the script
-    """
-    assert executable or file_, \
-        'Either "executable" or "file" attribute required'
-
-    returncode = execute(executable=executable, file_=file_,
-                         output=output, args=args)
-
-def pipe(executable=None, file_=None, input_=None, output=None,
-         args=None):
-    """Pipe the contents of a file through a program or shell script.
-    
-    :param executable: name of the executable to run
-    :param file\_: name of the script file, relative to the project directory,
-                  that should be run
-    :param input\_: name of the file containing the data that should be passed
-                   to the shell script on its standard input stream
-    :param output: name of the file to which the output of the script should be
-                   written
-    :param args: command-line arguments to pass to the script
-    """
-    assert executable or file_, \
-        'Either "executable" or "file" attribute required'
-    assert input_, 'Missing required attribute "input"'
-
-    returncode = execute(executable=executable, file_=file_,
-                         input_=input_, output=output, args=args)
 
 def execute(executable=None, file_=None, input_=None, output=None, args=None):
     """Generic external program execution.
