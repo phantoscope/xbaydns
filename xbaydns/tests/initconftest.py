39b2d8a803d81847220f9f87358e8ac743c9cb4a
huangdong <huangdong@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-26 07:48:21 +0000

    在openbsd下没有/var/named目录，所以backup的测试使用/etc来做测试。 :)

--- xbaydns/tests/initconftest.py
+++ xbaydns/tests/initconftest.py
@@ -95,7 +95,7 @@ class InitConfTest(basetest.BaseTestCase):
     def test_backup_conf(self):
         """测试backup_conf的调用"""
         tmpdir = self._create_dir("backuptest")
-        self.assertTrue( initconf.backup_conf("/var/named",os.path.join(self.basedir,tmpdir)) )
+        self.assertTrue( initconf.backup_conf("/etc",os.path.join(self.basedir,tmpdir)) )
         conffilename = "namedconf_%s.tar.bz2"%(time.strftime("%y%m%d%H%M"))
         log.debug("backup file is:%s"%(os.path.join(self.basedir,tmpdir,conffilename)))
         self.assertTrue( os.path.isfile(os.path.join(self.basedir,tmpdir,conffilename)) )
