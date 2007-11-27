87a739528110dc64d4d156446f1d18a13776b198
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-27 14:57:51 +0000

    建立了Acl,View,Domain相应的Model

--- xbaydnsweb/web/models.py
+++ xbaydnsweb/web/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Acl(models.Model):
+    """Acl Model"""
+    aclName = models.CharField(blank=True, maxlength=100)
+    aclMatch = models.CharField(blank=True, maxlength=100)
+    
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __str__(self):
+        return self.aclName
+        
+class View(models.Model):
+    """View Model"""
+    viewName = models.CharField(blank=True, maxlength=100)
+    matchClients = models.CharField(blank=True, maxlength=100)
+    tsigs = models.CharField(blank=True, maxlength=100)
+    
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __str__(self):
+        return self.viewName
+
+class Domain(models.Model):
+    """Domain Model"""
+    view = models.ForeignKey(View)
+    domains = models.CharField(blank=True, maxlength=100)
+
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __str__(self):
+        return "Domain"
