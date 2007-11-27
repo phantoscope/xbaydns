f69243124532a806180d1d565d2fbaf55435d716
huangdong <huangdong@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-27 01:34:37 +0000

    加入Mac OSX 10.5的操作系统适配

--- xbaydns/conf/sysconf.py
+++ xbaydns/conf/sysconf.py
@@ -6,12 +6,24 @@
 
 # 安装路径，是否能传进来？暂时写成根据相对路径
 import os
+import platform
+
+system, _, release, version, machine, processor = platform.uname()
+system, release, version = platform.system_alias(system, release,version)
+
 installdir = os.path.dirname(os.path.realpath(__file__)) + "/.."
 # 这里记录了bind启动的chroot根目录
 chroot_path = "/var/named"
 # 这里记录了named.conf所存储的路径
 namedconf = "/etc/namedb"
+
+if (system == 'Darwin'  and release == '9.1.0'):
+    #如果操作系统为Mac OSX 10.5
+    chroot_path = "/"
+    namedconf = "/etc"
+
 default_acl = dict(internal=('127.0.0.1', '10.217.24.0/24'))
 filename_map = dict(acl='acl/acldef.conf', defzone='defaultzone.conf')
 # 这里匹配了在不同的操作系统中使用的bind用户
 named_user_map = dict(freebsd='bind', openbsd='named', darwin='root')
+
