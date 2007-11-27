4e12525eff886c2b5742768914f204ca2c45fd13
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-27 17:12:41 +0000

    拆分了Model,用于保存数据

--- xbaydnsweb/web/models.py
+++ xbaydnsweb/web/models.py
@@ -3,35 +3,68 @@ from django.db import models
 class Acl(models.Model):
     """Acl Model"""
     aclName = models.CharField(blank=True, maxlength=100)
-    aclMatch = models.CharField(blank=True, maxlength=100)
     
     class Admin:
-        list_display = ('',)
+        list_display = ('aclName',)
         search_fields = ('',)
 
     def __str__(self):
         return self.aclName
         
+class AclMatch(models.Model):
+    """AclMatch Model"""
+    acl = models.ForeignKey(Acl)
+    aclMatch = models.CharField(blank=True, maxlength=100)
+
+    class Admin:
+        list_display = ('acl','aclMatch')
+        search_fields = ('',)
+
+    def __str__(self):
+        return "AclMatch"
+
 class View(models.Model):
     """View Model"""
     viewName = models.CharField(blank=True, maxlength=100)
-    matchClients = models.CharField(blank=True, maxlength=100)
-    tsigs = models.CharField(blank=True, maxlength=100)
     
     class Admin:
-        list_display = ('',)
+        list_display = ('viewName',)
         search_fields = ('',)
 
     def __str__(self):
         return self.viewName
 
+class ViewMatch(models.Model):
+    """ViewMatch Model"""
+    view = models.ForeignKey(View)
+    viewMatchClient = models.CharField(blank=True, maxlength=100)
+
+    class Admin:
+        list_display = ('view','viewMatchClient')
+        search_fields = ('',)
+
+    def __str__(self):
+        return "ViewMatch"
+        
+class ViewTsig(models.Model):
+    """ViewTsig Model"""
+    view = models.ForeignKey(View)
+    tsig = models.CharField(blank=True, maxlength=100)
+
+    class Admin:
+        list_display = ('view','tsig')
+        search_fields = ('',)
+
+    def __str__(self):
+        return "ViewTsig"
+
 class Domain(models.Model):
     """Domain Model"""
     view = models.ForeignKey(View)
-    domains = models.CharField(blank=True, maxlength=100)
+    domain = models.CharField(blank=True, maxlength=100)
 
     class Admin:
-        list_display = ('',)
+        list_display = ('view','domain')
         search_fields = ('',)
 
     def __str__(self):
