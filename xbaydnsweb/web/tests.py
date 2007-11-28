0d764617d1966daa1c0e086783e4e5bb62801e47
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-28 10:51:03 +0000

    忘记提交的web部分的unittest

---
+++ xbaydnsweb/web/tests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+tests.py
+
+Created by QingFeng on 2007-11-28.
+Copyright (c) 2007 yanxu. All rights reserved.
+"""
+
+#import basetest
+import logging.config
+import shutil
+import tempfile
+import time
+import unittest
+import base64
+
+log = logging.getLogger('xbaydnsweb.tests')
+logging.basicConfig(level=logging.DEBUG)
+
+from django.test.utils import *
+from django.test import TestCase
+from django.conf import settings
+from xbaydnsweb.web.models import *
+
+class ModelsTest(TestCase):
+    def setUp(self):
+        """初始化测试环境"""
+        self.acl1=Acl.objects.create(aclName='internal')
+        
+    def tearDown(self):
+        """清洁测试环境"""
+        pass
+
+    def test_Acl(self):
+        self.assertEquals(str(self.acl1), 'internal')
+        
+def suite():
+    """集合测试用例"""
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ModelsTest, 'test'))
+    return suite
+
+"""
+单独运行command的测试用例
+"""
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
+    
