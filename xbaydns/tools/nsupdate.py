46b4cba85aa18141e1039b8c1e4c957b6a9d9479
bg1tpt <bg1tpt@0f95f432-e13c-0410-b291-c5e6509ff455>
2007-11-29 07:19:33 +0000

    * don't import the names under dns, might be conflict. * self.domain should be a dns.name.Name object for compatibility.

--- xbaydns/tools/nsupdate.py
+++ xbaydns/tools/nsupdate.py
@@ -8,9 +8,7 @@ Copyright (c) 2007 xBayDNS Team. All rights reserved.
 
 update DNS server on FLY...
 """
-
-from dns import exception, name, query, rdataclass, rdatatype, rdataset,\
-                            rdata, resolver, tsigkeyring, update, zone
+import dns
 import logging.config
 
 log = logging.getLogger('xbaydns.tools.nsupdate')
@@ -22,29 +20,29 @@ class NSUpdate:
     def __init__(self, addr, domain, view = False, port = 53):
         self.addr = addr
         self.port = port
-        self.domain = domain
+        self.domain = dns.name.from_text(domain)
         self.view = view
         self.tsigkey = None
         if view != False:
             # get TSIG
-            self.tsigkey = tsigkeyring.from_text({'keyname' : 'xxxxxxxx'})
+            self.tsigkey = dns.tsigkeyring.from_text({'keyname' : 'xxxxxxxx'})
         self.domain_info = self._getDomainInfo()
-        self.updatemsg = update.Update(self.domain, keyring = self.tsigkey)
+        self.updatemsg = dns.update.Update(self.domain, keyring = self.tsigkey)
         
     def _getDomainInfo(self):
         # get full zone by xfr, for checking before add/remove/update records
         try:
-            domain_info = zone.from_xfr(query.xfr(self.addr, self.domain, keyring = self.tsigkey))
-        except query.BadResponse:
+            domain_info = dns.zone.from_xfr(dns.query.xfr(self.addr, self.domain, keyring = self.tsigkey))
+        except dns.query.BadResponse:
             log.error("DOMAIN XFR ERROR: Bad Response.")
             raise NSUpdateException("DOMAIN XFR ERROR: Bad Response.")
-        except zone.NoSOA:
+        except dns.zone.NoSOA:
             log.error("DOMAIN XFR ERROR: No SOA.")
             raise NSUpdateException("DOMAIN XFR ERROR: No SOA.")
-        except zone.NoNS:
+        except dns.zone.NoNS:
             log.error("DOMAIN XFR ERROR: No NS.")
             raise NSUpdateException("DOMAIN XFR ERROR: No NS.")
-        except exception.FormError:
+        except dns.exception.FormError:
             log.error("DOMAIN XFR ERROR: Form Error")
             raise NSUpdateException("DOMAIN XFR ERROR: Form Error")            
         return domain_info
@@ -53,13 +51,13 @@ class NSUpdate:
         for name, ttl, rdclass, rdtype, token in recordlist:
             rdatalist = []
             for token_str in token:
-                rdatalist.append(rdata.from_text(
-                        rdataclass.from_text(rdclass),
-                        rdatatype.from_text(rdtype),
+                rdatalist.append(dns.rdata.from_text(
+                        dns.rdataclass.from_text(rdclass),
+                        dns.rdatatype.from_text(rdtype),
                         token_str,
                         origin = self.domain))
                 log.debug("ADD RDATA: %s, %d, %s, %s, %s"%(name, ttl, rdclass, rdtype, token))
-            recordset = rdataset.from_rdata_list(ttl, rdatalist)
+            recordset = dns.rdataset.from_rdata_list(ttl, rdatalist)
             func(name, recordset)
                     
     def addRecord(self, recordlist):
@@ -91,46 +89,46 @@ class NSUpdate:
         send the update messages to NS server
         '''
         if usetcp == True:
-            query_wrapper = query.tcp
+            query_wrapper = dns.query.tcp
         else:
-            query_wrapper = query.udp
+            query_wrapper = dns.query.udp
         try:
             response = query_wrapper(self.updatemsg, self.addr, timeout, self.port)
-        except query.BadResponse:
+        except dns.query.BadResponse:
             log.error("UPDATE RESPONSE ERROR: Bad Response")
             raise NSUpdateException("UPDATE RESPONSE ERROR: Bad Response")
         log.debug("UPDATE RESPONSE: %s"%response)
-        self.updatemsg = update.Update(self.domain, keyring = self.tsigkey)
+        self.updatemsg = dns.update.Update(self.domain, keyring = self.tsigkey)
         
     def queryRecord(self, name, view = False, rdtype = 'A', 
                     usetcp = False, timeout = 30, rdclass = 'IN'):
         '''
         query a record though the specified NS server.
         '''
-        resolv = resolver.Resolver()
+        resolv = dns.resolver.Resolver()
         resolv.nameservers = [self.addr]
         resolv.port = self.port
         resolv.lifetime = timeout
         if view != False:
             # get TSIG
-            tsigkey = tsigkeyring.from_text({'keyname' : 'xxxxxxxx'})
+            tsigkey = dns.tsigkeyring.from_text({'keyname' : 'xxxxxxxx'})
             resolv.use_tsig(tsigkey)
         try:
-            resultset = resolv.query(name, rdatatype.from_text(rdtype), 
-                                    rdataclass.from_text(rdclass), tcp = usetcp)
-        except resolver.Timeout:
+            resultset = resolv.query(name, dns.rdatatype.from_text(rdtype), 
+                                    dns.rdataclass.from_text(rdclass), tcp = usetcp)
+        except dns.resolver.Timeout:
             # query time exceed the lifetime
             log.error("RESOLVER ERROR: Query Timeout.")
             raise NSUpdateException("RESOLVER ERROR: Query Timeout.")
-        except resolver.NXDOMAIN:
+        except dns.resolver.NXDOMAIN:
             # the query name does not exist
             log.error("RESOLVER ERROR: Name Not Exist.")
             raise NSUpdateException("RESOLVER ERROR: Name Not Exist.")
-        except resolver.NoAnswer:
+        except dns.resolver.NoAnswer:
             # the response did not contain an answer
             log.error("RESOLVER ERROR: No Answer.")
             raise NSUpdateException("RESOLVER ERROR: No Answer.")
-        except resolver.NoNameservers:
+        except dns.resolver.NoNameservers:
             # no non-broken nameservers are available to answer the question.
             log.error("RESOLVER ERROR: No Nameservers.")
             raise NSUpdateException("RESOLVER ERROR: No Nameservers.")
