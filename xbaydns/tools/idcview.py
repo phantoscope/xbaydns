c4f39baa7130b2244f6731c9cec56d677511ad34
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-03-18 09:49:49 +0000

    加入了一个简单的规则引擎,方便修改idcview中的判断规则

--- xbaydns/tools/idcview.py
+++ xbaydns/tools/idcview.py
@@ -10,18 +10,15 @@ Copyright (c) 2007 xBayDNS Team. All rights reserved.
 from decimal import Decimal
 from operator import itemgetter
 
-RULE={
-    'OUT_OF_REACH':
-}
-
-def pingtype_weight(pingtype,v,data_value=0):
+def pingtype_weight(pingtype,v,data_value=-1):
+    if data_value == -1:data_value=v
     pingtype=pingtype.upper()
-    if pingtype == 'OUT_OF_REACH':
-        return 0
-    if data_value!=0:
-        return (data_value+v)/2
-    return v
-
+    rule={
+        'OUT_OF_REACH':0,
+        'other':(data_value+v)/2,
+    }
+    return rule.get(pingtype,rule.get('other'))
+    
 def convfiles(files):
     data={}
     for f in files:
@@ -31,9 +28,9 @@ def convfiles(files):
                 continue
             ip,pingtype,pingavg,pingtime=map(lambda x:x.strip(),r.split(','))
             if ip not in data:
-                data[ip]=pingtype_weight(pingtype, Decimal(pingavg) )
+                data[ip]=pingtype_weight(pingtype,Decimal(pingavg),-1 )
             else:
-                data[ip]=pingtype_weight(pingtype, Decimal(pingavg),data[ip] )
+                data[ip]=pingtype_weight(pingtype,Decimal(pingavg),data[ip] )
     data=sorted(data.items(),key=itemgetter(1))
     return data
     
