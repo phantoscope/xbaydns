6fbd1c839d2ec721515a008ae059a6e50239a4ca
bg1tpt <bg1tpt@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-03-23 10:01:43 +0000

    返回结果转换回商集

--- xbaydns/tools/algorithms.py
+++ xbaydns/tools/algorithms.py
@@ -1,18 +1,30 @@
-from numarray import matrix
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+algorithms.py
+
+Created by Razor <bg1tpt AT gmail.com> on 2008-03-22.
+Copyright (c) 2008 xBayDNS Team. All rights reserved.
+
+"""
 
 def ecintersection(*devides):
     if len(devides) == 0:
         return 1
-    # construct set
+    
+    # construct complete set
     complete_set = set()
     for ec in devides[0]:
         for element in ec:
             complete_set.add(element)
-    print complete_set
+    
     ec_array = []
     ec_dict_template = {}
+    # construct ec dictionary templete
     for element in complete_set:
         ec_dict_template[element] = set()
+    
+    # construct ec for every element from the factor set
     for devide in devides:
         ec_dict = ec_dict_template.copy()
         for ec in devide:
@@ -22,8 +34,8 @@ def ecintersection(*devides):
             for element in ec:
                 ec_dict[element] = ec_dict[element].union(ec_set)
         ec_array.append(ec_dict)
-    #print ec_array
-    
+
+    # get the intersection of every element's ec
     ec_insect = {}
     for element in complete_set:
         for ec_dict in ec_array:
@@ -34,9 +46,19 @@ def ecintersection(*devides):
                 continue
             ec_insect[element].intersection_update(ec_dict[element])
     print ec_insect
-    
-    # constrct ec
-    
-# test
 
+    # convert to factor set
+    factor_set = []
+    i = 0
+    for ec in ec_insect.itervalues():
+        try:
+            if factor_set[i - 1] == ec:
+                continue
+        except IndexError:
+            pass
+        factor_set.append(ec)
+        i += 1
+    print factor_set
+
+# test
 ecintersection([[0,1], [2,3,4], [5]], [[0],[1,2], [3,4], [5]])
