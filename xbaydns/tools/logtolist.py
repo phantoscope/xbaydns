884a671bc9b6be4ff22ad03402dcf7b9d1d075e1
bg1tpt <bg1tpt@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-03-24 14:33:59 +0000

    add main function.

--- xbaydns/tools/logtolist.py
+++ xbaydns/tools/logtolist.py
@@ -6,7 +6,7 @@ logtolist.py
 Created by QingFeng on 2008-03-16.
 Copyright (c) 2008 xBayDNS Team. All rights reserved.
 """
-import re
+import re, sys
 
 def logtolist(s):
     data={}
@@ -15,3 +15,17 @@ def logtolist(s):
         data[ip]=''
     return data.keys()
 
+def main(logfile, iplist):
+    log = open(logfile, "r").read()
+    ips = logtolist(log)
+    iplistfile = open(iplist, "w")
+    for ip in ips:
+        iplistfile.write("%s\n"%ip)
+    iplistfile.close()
+    return
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Usage: %s logfile outputfile"%__file__
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
