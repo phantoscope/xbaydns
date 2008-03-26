55ae22e4dcc1324d85f001c93476705ea415a01f
bg1tpt <bg1tpt@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-03-26 03:13:35 +0000

    update key, use 10.210.132.70 as master for testing.

--- xbaydns/tools/install_agent.sh
+++ xbaydns/tools/install_agent.sh
@@ -12,8 +12,8 @@ GID=60190
 OSTYPE="`uname`"
 KEYBITS=2048
 INSTALLPATH="/data0/xbaydns"
-MASTER_PUB="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsGSPcihMTxXki3WCdTq+gTf5WlzC8uVFTDqloIeiwNEMJFpgEKYx28lsEiEElT1ovP260yPr3L02OsUeeq/rRG4ow4FZjH+1xoFYgRbfY0Juls5HKS3b8NotflOdZWmG85JXjC/fxSPpnmwjx3cS/q5WweXmzcA5h24fjZXp+xJJ5DZUAq9RlDtA/8ROZD4H6EUnu/KwGpvpb6p/pvOrcohw8Wk6m8gT6H8n6gt0XRsRPo0PUry9CzOxjiJl0/sz86aSU+NlSCnxmqigx6FfO2s4KpbVfV8kzsvlwW7X8aVUtU7NE3/d7VE+M8ZV2v/KR4+MUHJr9aYBhxbbe8aT1w== root@razor"
-MASTERIP="10.210.132.71"
+MASTER_PUB="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAr6BUi1BteZ4K8Jbvht1gmYK/pR6MKMSbXVgjb4y/J72NddfUwkbr6dAtdj6VBQiTNlsQaGOVEwZZDjbEAGoJhR3POngAlsIyjftgOI0kpIyH6LINOqrT2e9dpXx5uIZGY+3SaE8+is/THr12sWw87w2YIi+muQteP7dVCH499bEzzTRRz1FLz35t8kfdq9ptfJyeprBsKSnIntTyMc9tGbqojw7ReG7BITMqd6mbedNJ/Z2aKaf634puMmAyZ0tj14GlHHyQOrGOlFsP6DM/6QhOqfISJacb5kWJ4FhUyZ/aEcm4sNNfvHYWZdz6IkB7WUAryZizSBU7WmIQ7DIXLlfx6lp2s04dq5o2Ju8X8FUaO9rpvD6gUit4Q7Haefeml+JLW//TSf9GhJPpBTs5yeybb5jUxORlsoe39f/dhJyofZVcLCikHCxpyGbywbZPNKNSyypHzhTaGDWXVu0eEFho8WspLMqnPE2zpDP3woAB6fjpQfW9khEskLgrnsp8coE2tAT/Kt46Qn9JzbuIcaX9m3LmoP0GBei2JcbgkE+Qi0BEu62bs0LKmXL1lbL+roVkg8XBySV2e6Hildg9S20TyFJJyrDAbdB0qyQCjusMOs1FmrXBjJ8Lj/3bc2XmYMjdR5/Gw5kkag6CfwurG5vG9gC9N4vXpzmH+3Lvs2U="
+MASTERIP="10.210.132.70"
 
 if [ "`id -u`" != "0" ]; then
 	echo "Run this script using root user."
@@ -34,12 +34,14 @@ mkdir -p ${INSTALLPATH}/.ssh
 ssh-keygen -t rsa -b ${KEYBITS} -f ${INSTALLPATH}/.ssh/id_rsa -N ''
 echo "Please paste the public key to the master's authorized_keys (the line below):"
 cat ${INSTALLPATH}/.ssh/id_rsa.pub
+read -p "Be sure the public key is pasted, then press any key to continue" nouse
 
 # add the master's public key to authorized
 echo ${MASTER_PUB} > ${INSTALLPATH}/.ssh/authorized_keys
 
 # rsync agent
-# rsync -az -e 'ssh -i ${INSTALLPATH}/.ssh/id_rsa' ${USER}@${MASTERIP}:${INSTALLPATH}/agent/\* ${INSTALLPATH}
+mkdir -p ${INSTALLPATH}/iplatency
+rsync -az -e 'ssh -i ${INSTALLPATH}/.ssh/id_rsa' ${USER}@${MASTERIP}:${INSTALLPATH}/agent/\* ${INSTALLPATH}/iplatency
 
 # add to cron
 grep -v iplatency /etc/crontab > /tmp/crontab.xbaydns
--- xbaydns/tools/iplatency_agent.sh
+++ xbaydns/tools/iplatency_agent.sh
@@ -13,9 +13,9 @@ IPLATENCY_PATH="${INSTALL_DIR}/iplatency.py"
 CURDATE=`date "+%Y-%m-%d"`
 HOST_IP=`ifconfig | grep inet | head -n1 | awk '{print $2}'`
 OUTPUT_FILE=${HOST_IP}_${CURDATE}
-CONTROLLER_IP="10.210.132.71"
+CONTROLLER_IP="10.210.132.70"
 IPLST="/data0/xbaydns/iplst/iplst.txt"
-AGENT_PRIVATE_KEY="/data0/xbaydns/keys/agent"
+AGENT_PRIVATE_KEY="/data0/xbaydns/.ssh/id_rsa"
 
 mkdir -p ${OUTPUT_DIR}
 ssh -i ${AGENT_PRIVATE_KEY} iplatency@${CONTROLLER} cat ${IPLST} |
