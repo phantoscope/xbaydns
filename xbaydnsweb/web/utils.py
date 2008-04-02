5fce24c8755074a0f1ee474e5c4120a41445184f
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-02 10:01:42 +0000

    更新了应用配置的算法

--- xbaydnsweb/web/utils.py
+++ xbaydnsweb/web/utils.py
@@ -10,10 +10,31 @@ import logging.config
 from xbaydnsweb.web.models import *
 from xbaydns.dnsapi.namedconf import *
 from xbaydns.conf import sysconf
+from xbaydns.dnsapi import nsupdate
 
 log = logging.getLogger('xbaydnsweb.web.utils')
 #logging.basicConfig(level=logging.DEBUG)
 
+def record_nsupdate(record):
+    try:
+        nsupobj = nsupdate.NSUpdate('127.0.0.1',"%s."%record.domain,view="view_%s"%record.idc)
+        #['foo', 3600, 'IN', 'A', ['192.168.1.1', '172.16.1.1']]#record style
+        add_data=[[str(record.name),3600,'IN','A',record.ip],]
+        try:
+            record_a = nsupobj.queryRecord('%s.%s'%(record.name,record.domain), rdtype='A')
+            print "record_a",record_a
+            if len(record_a)!=0:
+                del_data=[[record.name,3600,'IN','A',record_a],]
+                nsupobj.removeRecord(del_data)
+        except:
+            print traceback.print_exc()
+        print add_data
+        nsupobj.addRecord(add_data)
+        nsupobj.commitChanges()
+    except:
+        print traceback.print_exc()
+        print "NSUpdate Error!"
+    return None
 def saveAllConf(path=sysconf.namedconf):
     nc = NamedConf()
     #将分组来源IP追加到一个ACL
@@ -35,18 +56,14 @@ def saveAllConf(path=sysconf.namedconf):
     nc.addDomain(domain_matchs)
     nc.save(path)
     nc.reload()
-
-# def saveAllConf(path=sysconf.namedconf):
-#     nc = NamedConf()
-#     for acl in Acl.objects.all():
-#         matchs=map(lambda x:x.aclMatch,
-#                 AclMatch.objects.filter(acl=acl))
-#         nc.addAcl(acl.aclName,matchs)
-#     for view in View.objects.all():
-#         view_matchs=[]
-#         for aclmatch in view.aclmatch.all():
-#             view_matchs.append(aclmatch.acl.aclName)
-#         nc.addView(view.viewName,view_matchs)
-#     domain_matchs = map(lambda x:x.zone,Domain.objects.all())
-#     nc.addDomain(domain_matchs)
-#     nc.save(path)
+    
+    class My(object):pass
+    for r in Record.objects.all():
+        records=Record.objects.filter(name=r.name,domain=r.domain,idc=r.idc)
+        m=My()
+        m.name,m.domain=r.name,r.domain
+        m.ip=map(lambda x:str(x.ip),records)
+        m.idc=r.idc.alias
+        print m.name,m.domain,m.idc,m.ip
+        record_nsupdate(m)
+    
