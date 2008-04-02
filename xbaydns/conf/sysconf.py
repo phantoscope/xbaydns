91a53807636c35a076a30bf78f9289dd51b33287
samhoo <samhoo@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-02 05:46:57 +0000

    add env setting support

--- xbaydns/conf/sysconf.py
+++ xbaydns/conf/sysconf.py
@@ -2,21 +2,6 @@
 # encoding: utf-8
 """
 这个文件中记录了所有的全局静态配置变量。现在只有named.conf所属路径和nameddb目录存储路径。
-可以使用环境变量来设置配置，环境变量定义如下：
-是否使用环境变量配置（YES为使用）
-XBAYDNS_ENV="YES"
-bind启动时的chroot目录，如果没有使用chroot设置为/
-XBAYDNS_CHROOT_PATH="/var/named"
-bind的配置文件路径
-XBAYDNS_BIND_CONF="/etc/namedb"
-bind的启动脚本
-XBAYDNS_BIND_START="/etc/rc.d/named start"
-bind的停止脚本
-XBAYDNS_BIND_STOP="/etc/rc.d/named stop"
-bind的重启脚本
-XBAYDNS_BIND_RESTART="/etc/rc.d/named restart"
-运行bind的用户
-XBAYDNS_BIND_USER="named"
 """
 
 
@@ -33,71 +18,85 @@ release = re.compile(r"^\d+.\d+").search(release).group()
 # 安装路径，是否能传进来？暂时写成根据相对路径
 installdir = os.path.dirname(os.path.realpath(__file__)) + "/.."
 # 这里记录了bind启动的chroot根目录
+
+#TODO: the following varirable should read from configuration ile
+#    : varible not defined in configuration should be set to ''
 chroot_path = "/var/named"
 # 这里记录了named.conf所存储的路径
 namedconf = "/etc/namedb"
+
+named_user = "bind"
+
 # 这是bind的启动脚本
 namedstart = "/etc/rc.d/named start"
 # 这是bind的停止脚本
 namedstop = "/etc/rc.d/named stop"
 # 这是bind的重启脚本
 namedrestart = "/etc/rc.d/named restart"
-# 运行bind的用户
-named_user = "named"
 
-if (os.getenv("XBAYDNS_ENV") == "YES"):
-    #通过环境变量，自定义适配
-    if (os.getenv("XBAYDNS_CHROOT_PATH","") != ""):
-        chroot_path = os.getenv("XBAYDNS_CHROOT_PATH")
-    if (os.getenv("XBAYDNS_BIND_CONF","") != ""):
-        namedconf = os.getenv("XBAYDNS_BIND_CONF")
-    if (os.getenv("XBAYDNS_BIND_START","") != ""):
-        namedstart = os.getenv("XBAYDNS_BIND_START")
-    if (os.getenv("XBAYDNS_BIND_STOP","") != ""):
-        namedstop = os.getenv("XBAYDNS_BIND_STOP")
-    if (os.getenv("XBAYDNS_BIND_RESTART","") != ""):
-        namedstop = os.getenv("XBAYDNS_BIND_RESTART")
-    if (os.getenv("XBAYDNS_BIND_USER","") != ""):
-        named_user = os.getenv("XBAYDNS_BIND_USER")
-elif (system == 'Darwin'):
-    #操作系统为Mac OSX
-    chroot_path = "/"
-    namedconf = "/etc"
-    named_user = "root"
-    namedstart = "sudo service org.isc.named start;sleep 2"
-    namedstop = "sudo service org.isc.named stop"
-    if (release == '9.1.0'):
-        #OSX 10.5&10.5.1
-        pass
-elif (system == "FreeBSD"):
-    #操作系统为FreeBSD
-    chroot_path = "/var/named"
-    namedconf = "/etc/namedb"
-    named_user = "bind"
-    if (release >= "6.2"):
-        pass
-    else:
-        raise "Unsupported release."
-elif (system == "OpenBSD"):
-    # 操作系统为OpenBSD
-    named_user = "named"
-    chroot_path = "/var/named"
-    namedconf = "/etc"
-    if (release >=  "4.2"):
-        pass
-    else:
-        raise "Unsupported release."
-elif (system == "Linux"):
-    # 操作系统为Linux
-    named_user = "bind"
-    chroot_path = "/"
-    namedconf = "/etc/bind"
+chroot_path = os.getenv('XBAYDNS_CHROOT_PATH', chroot_path)
+namedconf  = os.getenv('XBAYDNS_BIND_CONF', namedconf)
+named_user  = os.getenv('XBAYDNS_BIND_USER', named_user)
+namedstart = os.getenv('XBAYDNS_BIND_START', namedstart)
+namedstop = os.getenv('XBAYDNS_BIND_STOP', namedstop)
+namedrestart = os.getenv('XBAYDNS_BIND_RESTART', namedrestart)
+
+namedef = {'Darwin':
+                {'chroot_path':'/', 
+                'namedconf':'/etc', 
+                'named_user':'root', 
+                'namedstart':'sudo service org.isc.named start;sleep 2', 
+                'namedstart':'sudo service org.isc.named restart;sleep 2', 
+                'namedstop':'sudo service org.isc.named stop'},
+           'FreeBSD':
+                {'chroot_path':'/var/named', 
+                'namedconf':'/etc/namedb', 
+                'named_user':'bind', 
+                'namedstart':'/etc/rc.d/named start',
+                'namedrestart':'/etc/rc.d/named restart',
+                'namedstop':'/etc/rc.d/named stop'},
+           'OpenBSD':
+                {'chroot_path':'/var/named', 
+                'namedconf':'/var/named', 
+                'named_user':'named', 
+                'namedstart':'/etc/rc.d/named start',
+                'namedrestart':'/etc/rc.d/named restart',
+                'namedstop':'/etc/rc.d/named stop'},
+           'Linux':
+                {'chroot_path':'/', 
+                'namedconf':'/etc/bind', 
+                'named_user':'bind', 
+                'namedstart':'/etc/rc.d/named start',
+                'namedrestart':'/etc/rc.d/named restart',
+                'namedstop':'/etc/rc.d/named stop'}}
+
+if     ((system == 'Darwin') and (release == '9.1.0')) \
+    or ((system == 'FreeBSD') and (release >= '6.2')) \
+    or ((system == 'OpenBSD') and (release >= '4.2')) \
+    or (system == 'Linux'): 
+        if len(chroot_path) == 0:
+            chroot_path = namedef[system]['chroot_path']
+        if len(namedconf) == 0:
+            namedconf = namedef[system]['namedconf']
+        if len(named_user) == 0:
+            named_user = namedef[system]['named_user']
+        if len(namedstart) == 0:
+            namedstart = namedef[system]['namedstart']
+        if len(namedstop) == 0:
+            namedstop = namedef[system]['namedstop']
+        if len(namedrestart) == 0:
+            namedrestart = namedef[system]['namedrestart']
+else:
+    raise "Unsupported release."
+
 try:
     named_uid = pwd.getpwnam(named_user)[2]
+
 except KeyError:
     print "No such a user %s. I'll exit."%named_user
-    sys.exit(-1)
+    sys.exit(errno.EINVAL)
         
+
 default_acl = dict(internal=('127.0.0.1', '10.217.24.0/24'))
 filename_map = dict(acl='acl/acldef.conf')
 default_zone_file = "defaultzone.conf"
