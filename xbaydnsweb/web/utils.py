03c22879bfbe61d78225049d4e3a16c845cd4300
paradise.qingfeng <paradise.qingfeng@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-03 11:31:22 +0000

    加入了默认机房的record更新,并优化了程序结构,重置了数据库

--- xbaydnsweb/web/utils.py
+++ xbaydnsweb/web/utils.py
@@ -21,6 +21,7 @@ log = logging.getLogger('xbaydnsweb.web.utils')
 #logging.basicConfig(level=logging.DEBUG)
 
 def record_nsupdate(record):
+    """调用NSUpdate更新record"""
     try:
         nsupobj = nsupdate.NSUpdate('127.0.0.1',"%s."%record.domain,view=record.viewname)
         #['foo', 3600, 'IN', 'A', ['192.168.1.1', '172.16.1.1']]#record style
@@ -42,8 +43,8 @@ def record_nsupdate(record):
         print "NSUpdate Error!"
     print "NSUpdate OK"
 
-#生成计算碎集所需要的数据结构
 def genResult():
+    """生成计算碎集所需要的数据结构"""
     result,ips={},[]
     for record in Record.objects.all():
         for r in Result.objects.filter(record=record):
@@ -63,8 +64,8 @@ def genResult():
         ips.append( domainip )
     return algorithms.ecintersection(*ips)
     
-#将Result的结果按照域名合并汇总并返回
 def getResults(ip):
+    """将Result的结果按照域名合并汇总并返回"""
     records={}
     for result in Result.objects.filter(ip=ip):
         k="%s.%s"%(result.record.name,result.record.domain)
@@ -72,10 +73,34 @@ def getResults(ip):
             records[k]=[]
         records[k].append(str(result.record.ip))
     return records
-    
-#保存所有配置,生成所有bind需要的配置文件
-def saveAllConf(path=sysconf.namedconf):
-    map(lambda x:x.delete(),IPArea.objects.all())
+
+def updateDomain():
+    """发出nsupdate请求,更新所有record和更新默认机房的记录"""
+    class My(object):pass
+    for view in IPArea.objects.all():
+        ip=eval(view.ip)[0]
+        records=getResults(ip)
+        print "view,ip",view.view,ip
+        for k,ips in records.items():
+            print "k,ips",k,ips
+            m=My()
+            name=k.split('.')
+            m.name,m.domain=name[0],'.'.join(name[1:])
+            m.ip=ips
+            m.viewname=view.view
+            print m.name,m.domain,m.viewname,m.ip
+            record_nsupdate(m)
+    """更新默认机房的记录"""
+    for r in Record.objects.filter(is_defaultidc=True):
+        m=My()
+        m.name,m.domain=r.name,r.domain
+        m.ip=[str(r.ip),]
+        m.viewname="view_default"
+        print m.name,m.domain,m.viewname,m.ip
+        record_nsupdate(m)
+
+def genNamedConf(path):
+    """生成所有named配置文件"""
     nc = NamedConf()
     result=genResult()
     if result!=1:
@@ -94,19 +119,11 @@ def saveAllConf(path=sysconf.namedconf):
     domain_matchs = map(lambda x:'%s'%x.domain,Record.objects.all())
     nc.addDomain(domain_matchs)
     nc.save(path)
-    nc.reload()
+    nc.reload()    
+        
+#保存所有配置,生成所有bind需要的配置文件
+def saveAllConf(path=sysconf.namedconf):
+    map(lambda x:x.delete(),IPArea.objects.all())
+    genNamedConf(path)
+    updateDomain()
     
-    class My(object):pass
-    for view in IPArea.objects.all():
-        ip=eval(view.ip)[0]
-        records=getResults(ip)
-        print "view,ip",view.view,ip
-        for k,ips in records.items():
-            print "k,ips",k,ips
-            m=My()
-            name=k.split('.')
-            m.name,m.domain=name[0],'.'.join(name[1:])
-            m.ip=ips
-            m.viewname=view.view
-            print m.name,m.domain,m.viewname,m.ip
-            record_nsupdate(m)
