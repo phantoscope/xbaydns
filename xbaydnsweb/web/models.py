45193c7b19e5cebda1fcb2456b35aee57e38962e
nemoliu <nemoliu@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-10 05:26:28 +0000

    机房改为必填

--- xbaydnsweb/web/models.py
+++ xbaydnsweb/web/models.py
@@ -98,7 +98,7 @@ class Record(models.Model):
     """Record Model"""
     name = models.CharField(max_length=100,verbose_name=_('record_name_verbose_name'),help_text='例如:www')
     domain = models.ForeignKey(Domain,verbose_name=_('record_domain_verbose_name'))
-    idc = models.ForeignKey(IDC,verbose_name=_('record_idc_verbose_name'),blank=True)
+    idc = models.ForeignKey(IDC,verbose_name=_('record_idc_verbose_name'))
     record_type = models.ForeignKey(RecordType,verbose_name=_('record_type_name'))
     record_info = models.CharField(max_length=100,verbose_name=_('record_info_name'))
     is_defaultidc = models.BooleanField(default=False,verbose_name=_('record_is_defaultidc_verbose_name'))
