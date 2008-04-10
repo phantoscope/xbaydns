4a9ea071b02852e154955d82123e9f0335884b57
samhoo <samhoo@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-10 12:23:58 +0000

    update named conf by root

--- xbaydns/tools/slave/UpdateNamed.sh
+++ xbaydns/tools/slave/UpdateNamed.sh
@@ -8,11 +8,25 @@ if [ -f "$PPATH/../slave.conf" ]; then
 fi
 
 cd $PPATH/..
-rsync -avz -e 'ssh -i /home/xdslave/rsync-key' \
- xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/acl $XBAYDNS_CHROOT_PATH/etc/
 
 rsync -avz -e 'ssh -i /home/xdslave/rsync-key' \
- xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/view $XBAYDNS_CHROOT_PATH/etc/
+ xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/acl /home/xdslave/named/etc/
+
+if ! diff /home/xdslave/named/etc/acl  $XBAYDNS_CHROOT_PATH/etc/acl  > /dev/null 2>&1; then
+    rsync /home/xdslave/named/etc/acl $XBAYDNS_CHROOT_PATH/etc/
+    touch need_reload
+fi
+
 
 rsync -avz -e 'ssh -i /home/xdslave/rsync-key' \
- xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/dynamic $XBAYDNS_CHROOT_PATH/etc/
+ xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/view  /home/xdslave/named/etc/
+
+if ! diff /home/xdslave/named/etc/view  $XBAYDNS_CHROOT_PATH/etc/view  > /dev/null 2>&1; then
+    rsync /home/xdslave/named/etc/view $XBAYDNS_CHROOT_PATH/etc/
+    touch need_reload
+fi
+
+if [ -f need_reload ]; then
+   rndc reload
+   rm need_reload
+fi
