49cc8c530ef518be47c24e9ab82c7286a9400a73
nemoliu <nemoliu@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-15 07:56:20 +0000

    在domain中加入soa相关信息修改

--- xbaydnsweb/web/models.py
+++ xbaydnsweb/web/models.py
@@ -168,10 +168,10 @@ def isDuplicateRecord(field_data,all_data):
 class Record(models.Model):
     """Record Model"""
     name = models.CharField(max_length=100,verbose_name=_('record_name_verbose_name'),help_text='例如:www',validator_list=[isDuplicateRecord])
-    domain = models.ForeignKey(Domain,verbose_name=_('record_domain_verbose_name'))
-    idc = models.ForeignKey(IDC,verbose_name=_('record_idc_verbose_name'))
+    domain = models.ForeignKey(Domain,verbose_name=_('record_domain_verbose_name'),edit_inline=models.STACKED, num_in_admin=1)
+    idc = models.ForeignKey(IDC,verbose_name=_('record_idc_verbose_name'),core=True)
     record_type = models.ForeignKey(RecordType,verbose_name=_('record_type_name'))
-    record_info = models.CharField(max_length=100,verbose_name=_('record_info_name'))
+    record_info = models.CharField(max_length=100,verbose_name=_('record_info_name'),core=True)
     is_defaultidc = models.BooleanField(default=False,verbose_name=_('record_is_defaultidc_verbose_name'))
     ttl = models.IntegerField(verbose_name=_('record_ttl_verbose_name'))
 
