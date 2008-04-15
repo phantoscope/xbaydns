d5bd5e311e5d18a66c291bdff0c4b771db84b865
samhoo <samhoo@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-04-15 15:01:29 +0000

    fix root sync

--- xbaydns/tools/slave/ReloadNamed.sh
+++ xbaydns/tools/slave/ReloadNamed.sh
@@ -9,18 +9,10 @@ fi
 
 cd $PPATH/..
 
-rsync -avz -e 'ssh -i /home/xdslave/rsync-key' \
- xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/acl /home/xdslave/named/etc/
-
 if ! diff /home/xdslave/named/etc/acl  $XBAYDNS_CHROOT_PATH/etc/acl  > /dev/null 2>&1; then
     rsync -avz /home/xdslave/named/etc/acl $XBAYDNS_CHROOT_PATH/etc/
     touch need_reload
 fi
-
-
-rsync -avz -e 'ssh -i /home/xdslave/rsync-key' \
- xbaydns\@$MASTER_IP:/home/xbaydns/slave/named/etc/view  /home/xdslave/named/etc/
-
 
 
 if ! diff /home/xdslave/named/etc/view  $XBAYDNS_CHROOT_PATH/etc/view  > /dev/null 2>&1; then
