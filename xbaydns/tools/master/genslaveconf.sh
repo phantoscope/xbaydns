d15c8e721327b34ca3f942ef056aa5c4101bf825
samhoo <samhoo@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-05-09 05:24:07 +0000

    more replacement when generate view for slave

--- xbaydns/tools/master/genslaveconf.sh
+++ xbaydns/tools/master/genslaveconf.sh
@@ -2,16 +2,25 @@
 
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
 source /home/xbaydns/xdenv
-#TODO
 source /home/xbaydns/master.conf
 
-env|grep XBAY
 export MASTER_PATH=$XBAYDNS_CHROOT_PATH/etc
 export SLAVE_PATH=/home/xbaydns/slave/named/etc
-echo "SLAVE_PATH",$SLAVE_PATH
+
+slave_replace()
+{
+cat $1 | sed "s/server .* { keys \(.*\) };[ ]*$/server $MASTER_IP { keys \1 };/g" | sed "s/type master;/type slave;\n        masters{ $MASTER_IP; };/g" | uniq >$1.out
+mv $1.out $1
+}
+
 mkdir -p $SLAVE_PATH
+rm -f $SLAVE_PATH/acl/*
 cp -rf $MASTER_PATH/acl $SLAVE_PATH
+
+rm -f $SLAVE_PATH/view/*
 cp -rf $MASTER_PATH/view $SLAVE_PATH
-cp -rf $MASTER_PATH/dynamic $SLAVE_PATH
-find $SLAVE_PATH/view/* | xargs -Iaa sed -i.master s/"type master;"/"type slave;\n        masters{ $MASTER_IP; };"/g aa
-rm $SLAVE_PATH/view/*.master
+
+for i in `find $SLAVE_PATH/view/*`
+do
+ slave_replace $i
+done
