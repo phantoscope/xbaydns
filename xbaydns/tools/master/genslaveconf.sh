713b57ce646ff2030ec69abb72a0cb2582bec93e
samhoo <samhoo@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-05-12 16:52:47 +0000

    fix bug: replace "type master;" only once

--- xbaydns/tools/master/genslaveconf.sh
+++ xbaydns/tools/master/genslaveconf.sh
@@ -8,11 +8,13 @@ fi
 
 export MASTER_PATH=${XBAYDNS_CHROOT_PATH}/etc
 export SLAVE_PATH=${XDPREFIX}/home/xbaydns/slave/named/etc
-echo "SLAVE_PATH",$SLAVE_PATH
+
 mkdir -p $SLAVE_PATH
 rm -f $SLAVE_PATH/acl/*
 cp -rf $MASTER_PATH/acl $SLAVE_PATH
 rm -f $SLAVE_PATH/view/*
 cp -rf $MASTER_PATH/view $SLAVE_PATH
-find $SLAVE_PATH/view/ -type f | xargs -Iaa sed -i.master -e s/"type master;"/"type slave;\n        masters{ ${MASTER_IP}; };"/g -e "s/server .* { keys \(.*\) };[ ]*$/server ${MASTER_IP} { keys \1 };/g" -e "N;/server .* { keys \(.*\) };[ ]*$/D" aa
+
+find $SLAVE_PATH/view/ -type f | xargs -Iaa sed -i.master -e "s/type master;/type slave;\n        masters{ ${MASTER_IP}; };/g" aa
+find $SLAVE_PATH/view/ -type f | xargs -Iaa sed -i.master -e "s/server .* { keys \(.*\) };[ ]*$/server ${MASTER_IP} { keys \1 };/g" -e "N;/server .* { keys \(.*\) };[ ]*$/D" aa
 rm $SLAVE_PATH/view/*.master
