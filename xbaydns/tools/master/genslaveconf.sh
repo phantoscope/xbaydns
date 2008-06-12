25f666f2c7fe04e63cfe9f6c71c4662b9183c741
quakelee <quakelee@0f95f432-e13c-0410-b291-c5e6509ff455>
2008-06-12 03:10:51 +0000

    fixed two bugs in genslaveconf. The one didn't compatible with old version xargs. Another one can make a lot of wrong file with vim swp file.

--- xbaydns/tools/master/genslaveconf.sh
+++ xbaydns/tools/master/genslaveconf.sh
@@ -15,6 +15,6 @@ cp -rf $MASTER_PATH/acl $SLAVE_PATH
 rm -f $SLAVE_PATH/view/*
 cp -rf $MASTER_PATH/view $SLAVE_PATH
 
-find $SLAVE_PATH/view/ -type f | xargs -Iaa sed -i.master -e "s/type master;/type slave;\n        masters{ ${MASTER_IP}; };/g" aa
-find $SLAVE_PATH/view/ -type f | xargs -Iaa sed -i.master -e "s/server .* { keys \(.*\) };[ ]*$/server ${MASTER_IP} { keys \1 };/g" -e "N;/server .* { keys \(.*\) };[ ]*$/D" aa
+find $SLAVE_PATH/view/ -type f -name "*" | xargs sed -i.master -e "s/type master;/type slave;\n        masters{ ${MASTER_IP}; };/g"
+find $SLAVE_PATH/view/ -type f -name "*" | xargs sed -i.master -e "s/server .* { keys \(.*\) };[ ]*$/server ${MASTER_IP} { keys \1 };/g" -e "N;/server .* { keys \(.*\) };[ ]*$/D"
 rm $SLAVE_PATH/view/*.master
